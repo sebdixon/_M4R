a783f99a962c3c4ed8ad46062a2b28d458929ab8
Sebastian Dixon <sjd220@ic.ac.uk>
2024-02-21 10:18:36 +0000

    cleaning + updates

--- pileup/inputs.py
+++ pileup/inputs.py
@@ -1,6 +1,9 @@
 import numpy as np
 
+# Load the data
 ARF = np.loadtxt('txt_inputs/arf.txt')       # Effective area
 ENERGY_BINS = np.loadtxt('txt_inputs/energy_bins.txt')
 RMF = np.loadtxt('txt_inputs/rmf.txt')
-RMF = (RMF.T / RMF.sum(axis=1)).T # Need to normalise RMF
+
+RMF = (RMF.T / RMF.sum(axis=1)).T
+
