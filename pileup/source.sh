fc73d2a29d184f1f2378f2137a15e6d4c7e9fe05
Sebastian Dixon <sjd220@ic.ac.uk>
2024-05-16 12:19:43 +0100

    ciao stuff

--- pileup/source.sh
+++ pileup/source.sh
@@ -1,31 +1,49 @@
 #!/bin/bash
+# to execute:
+# chmod +x source.sh
+# ./source.sh
 
-VENV_CIAO="ciao/bin/activate"
-VENV_BASE="base/bin/activate"
+# This script runs the necessary steps for SNPE in base, and
+# simulates the data in ciao. The script is run in the following order:
+# 1. source_ciao.py in ciao
+# 2. source_base.py in base
+# 3. source_ciao.py in ciao
+# 4. source_base.py in base
+
+CONDA_PATH="/Users/sebastiandixon/opt/anaconda3"
+VENV_CIAO="ciao"
+VENV_BASE="base"
 
 SCRIPT_BASE="source_base.py"
 SCRIPT_CIAO="source_ciao.py"
 
 ROUND=1
+NUM_SIMS=10
+NUM_ROUNDS=10
 
 run_in_ciao() {
-    source $VENV_CIAO
+    source $CONDA_PATH/bin/activate $VENV_CIAO
     export ROUND
+    export NUM_SIMS
     echo "Running $1 in ciao with ROUND=$ROUND"
     python $1
-    deactivate
-    ROUND=$((ROUND + 1))
+    conda deactivate
 }
 
+# Function to run a script in the 'base' virtual environment
 run_in_base() {
-    source $VENV_BASE
-    unset ROUND
+    source $CONDA_PATH/bin/activate $VENV_BASE
+    export ROUND
+    export NUM_SIMS
     echo "Running $1 in base"
     python $1
-    deactivate
+    conda deactivate
 }
 
-run_in_ciao $SCRIPT_BASE
-run_in_base $SCRIPT_CIAO
-run_in_ciao $SCRIPT_BASE
-run_in_base $SCRIPT_CIAO
+
+for ((i=1; i<=NUM_ROUNDS; i++))
+do
+    run_in_ciao $SCRIPT_CIAO
+    run_in_base $SCRIPT_BASE
+    ROUND=$((ROUND + 1))
+done
